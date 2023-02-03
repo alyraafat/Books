92c5a41c958bf375fa79c409388ec748eb8abe4a
aly raafat <alewaraafat@gmail.com>
2023-02-03 22:12:24 +0200

    app and book list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-      
+      <BookList/>
+      <BookCreate/>
     </div>
   );
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import useBooksContext from '../hooks/use-books-context';
+import BookShow from './BookShow';
 function BookList() {
+  const { books } = useBooksContext()
+  const renderedbooks = books.map((book)=>{
+    <BookShow book={book}/>
+  })
   return (
     <div>
-      
+      {renderedbooks}
     </div>
   )
 }
